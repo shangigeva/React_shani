fbbe8fe8e885d3b924b7fb79f92046c877fa4f76
shani geva <shanig7@gmail.com>
2023-12-02 16:39:06 +0200

    remove unused

--- src/layout/footer/FooterComponent.jsx
+++ src/layout/footer/FooterComponent.jsx
@@ -1,10 +1,8 @@
 import { Fragment, useState } from "react";
-import { BottomNavigation, Divider } from "@mui/material";
+import { Divider } from "@mui/material";
 import Links from "./ui/Links";
 
 const FooterComponent = () => {
-  const [value, setValue] = useState(0);
-
   return (
     <Fragment>
       <Divider></Divider>
